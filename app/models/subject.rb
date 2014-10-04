c91085035bd4491acd8e39bb61bdc59277af4d6c
sourav moitra <astrosourav@gmail.com>
2014-10-04 21:09:53 +0530

    added validations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,12 @@
 class Subject < ActiveRecord::Base
   has_many :sub_str_maps
+  validates :name, presence: true, length: { in: 4..20 }
+
+  before_validation :strip_strings
+
+  private
+
+  def strip_strings
+    self.name = name.strip
+  end
 end
